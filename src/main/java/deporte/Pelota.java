59219ce9f5c5f9beff38e13096f5e03b78b57929
user <user@PC514>
2020-11-18 11:53:13 +0100

    Push de todo 2

---
+++ src/main/java/deporte/Pelota.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//FullNameQuality - Nombre cualitativo que pone JVM (package deperote.Pelota)
+
+package deporte;
+
+/**
+ *
+ * @author user
+ */
+public class Pelota {
+    private int x, y;
+}
